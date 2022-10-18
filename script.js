3633e7d228d88472e9437b456bd96e881422e460
Neolivan <neolivan.eng@outlook.com>
2022-10-17 22:57:22 -0300

    upload project

---
+++ script.js
@@ -0,0 +1,62 @@
+
+let pokemonList = []
+
+
+async function getPokeList(){
+   await fetch("https://pokeapi.co/api/v2/pokemon/?limit=1154").then((T) => T.json())
+    .then((resp) => {
+      pokemonList = resp.results
+    })
+
+}
+
+async function getPkemon(){
+    let index = Math.floor(Math.random() * 1151) + 1;
+    if(index > 905){
+        if(index < 10001){
+            if(index < 1000){
+                index = 905
+            }else{
+                index = 10001
+            }
+        }
+    }
+    let shineLucky =  Math.floor(Math.random() * 11)
+    let shine = shineLucky == 1? true : false
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${index}/`).then((T) => T.json())
+    .then((resp) => {
+        let pokeImg = resp.sprites.front_default
+        if(shine){
+            document.getElementById('poke-name').innerHTML = `⭐ ${pokemonList[index -1].name} ⭐`;
+        }else{
+            document.getElementById('poke-name').innerHTML = pokemonList[index -1].name;
+        }
+        if(pokeImg){
+            console.log(shine)
+            if(shine){
+                document.getElementById('poke-img').setAttribute('src', resp.sprites.front_shiny)
+            }else{
+                document.getElementById('poke-img').setAttribute('src', pokeImg)
+            }
+        }else{
+            document.getElementById('poke-img').setAttribute('src', './assets/pokemon.jpg')
+
+        }
+    })
+}
+
+async function _init_(){
+    await getPokeList()
+    getPkemon()
+
+}
+
+async function refresh(){
+    console.log('refresh')
+    await getPkemon()
+}
+
+_init_()
+
+document.querySelector('#refresh').addEventListener("click",refresh);
+
